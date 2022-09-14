a1cb16ca298d996df3cc2fbf1b78ec996b3f6e92
drewp@bigasterisk.com <drewp@bigasterisk.com>
2022-09-13 22:32:50 -0700

    fuss with ns and stuff to get it working. forward all (over http) to nginx at first

--- switch_to_nginx.sh
+++ switch_to_nginx.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
 cd /my/serv/pomerium
-kubectl delete -f kube
+
+# not all this, since it includes Certs and also the ns and CRD for the Certs
+kubectl delete -f kube/10-pomerium.yaml
+kubectl delete -f kube/20-deployment.yaml
+kubectl delete -f kube/03-volumes.yaml
 
 cd /my/serv/nginx
 /my/proj/release/env/bin/invoke run
--- switch_to_pomerium.sh
+++ switch_to_pomerium.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
-cd /my/serv/nginx
-skaffold delete -f wrapped_skaffold.yaml
+#cd /my/serv/nginx
+#skaffold delete -f wrapped_skaffold.yaml
+
 
 cd /my/serv/pomerium
-kubectl apply -f kube/
-#kubectl create secret tls megasecond-club-tls --namespace=pomerium --cert=./megasecond.club.pem --key=./megasecond.club-key.pem
-#kubectl create secret tls photo-bigasterisk-com-tls --namespace=pomerium --cert=./photo.bigasterisk.com.pem --key=./photo.bigasterisk.com-key.pem
+kubectl apply -f kube/03-volumes.yaml
+kubectl apply -f kube/10-pomerium.yaml
+kubectl apply -f kube/20-deployment.yaml
